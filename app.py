ceb205ad767dfbecc6dc4bc76157e3dc79423600
Alicia426 <you@example.com>
2021-03-24 03:14:20 -0600

    Added multithreading in a basic manner, I cant sleep, but I can sure as hell code

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Minimal version of Duino-Coin PC Miner, useful for developing own apps. Created by revox 2020-2021
 # Modified by Alicia426 to run ina docker container and be able to be started with a URL
-## Addign multiprocessing to start and stop the mining thread
+# Addign multiprocessing to start and stop the mining thread
 import socket
 import hashlib
 import urllib.request
@@ -17,27 +17,26 @@ babylog.start()
 
 app = Flask(__name__)
 
-soc = socket.socket()
-soc.settimeout(10)
-
 
 class Miner:
 
-    def __init__(self,username, UseLowerDiff):
-       self.username=username
-       self.UseLowerDiff=UseLowerDiff
+    def __init__(self, username, UseLowerDiff):
+        self.username = username
+        self.UseLowerDiff = UseLowerDiff
+        self.soc = socket.socket()
+        self.soc.settimeout(10)
 
     def mine(self):
         current_buffer = ''
         if self.UseLowerDiff:
-            soc.send(
+            self.soc.send(
                 bytes("JOB," + str(self.username) + ",MEDIUM", encoding="utf8")
             )  # Send job request for lower difficulty
         else:
-            soc.send(
+            self.soc.send(
                 bytes("JOB," + str(self.username), encoding="utf8")
             )  # Send job request
-        job = soc.recv(1024).decode()  # Get work from pool
+        job = self.soc.recv(1024).decode()  # Get work from pool
         # Split received data to job (job and difficulty)
         job = job.split(",")
         difficulty = job[2]
@@ -48,11 +47,11 @@ class Miner:
                 str(job[0] + str(result)).encode("utf-8")
             ).hexdigest()  # Generate hash
             if job[1] == ducos1:  # If result is even with job
-                soc.send(
+                self.soc.send(
                     bytes(str(result) + ",,Minimal_PC_Miner", encoding="utf8")
                 )  # Send result of hashing algorithm to pool
                 # Get feedback about the result
-                feedback = soc.recv(1024).decode()
+                feedback = self.soc.recv(1024).decode()
                 if feedback == "GOOD":  # If result was good
                     current_buffer = "Accepted share: " + \
                         str(result)+' '+"Difficulty: "+str(difficulty)
@@ -62,7 +61,6 @@ class Miner:
                         str(result)+' '+"Difficulty: "+str(difficulty)
                     break
         return current_buffer
-
 
     def requestAndMine(self):
         try:
@@ -77,8 +75,8 @@ class Miner:
 
             # This section connects and logs user to the server
             # Connect to the server
-            soc.connect((str(pool_address), int(pool_port)))
-            server_version = soc.recv(3).decode()  # Get server version
+            self.soc.connect((str(pool_address), int(pool_port)))
+            server_version = self.soc.recv(3).decode()  # Get server version
             print("Server is on version", server_version)
             # Mining section
             while True:
@@ -94,31 +92,29 @@ class Miner:
             babylog.error("Error occured: " + str(e) + ", restarting in 5s.")
             time.sleep(5)
             self.requestAndMine()
-        
+
     def start_mining(self):
         """Starts mining as a process"""
         try:
-            self.proc.terminate() # pylint: disable=access-member-before-definition
-        except AttributeError:
-            pass
+            self.proc.terminate()  # pylint: disable=access-member-before-definition
+        except Exception:
+            babylog.status('No previously running threads, OK!')
         finally:
-            self.proc = multiprocessing.Process( # pylint: disable=attribute-defined-outside-init
+            self.proc = multiprocessing.Process(  # pylint: disable=attribute-defined-outside-init
                 target=self.requestAndMine, args=())
-
             self.proc.start()
 
     def stop_mining(self):
         """Stops mining as a process"""
         try:
-            self.proc.terminate()
+            self.proc.terminate()  # pylint: disable=access-member-before-definition
         except Exception as e:
             babylog.error(str(e))
 
 
-
-
-
 # Global mining object
+
+miners = []
 
 
 @app.route('/mine/<username>/<UseLowerDiff>')
@@ -130,18 +126,21 @@ def mining(username, UseLowerDiff):
     # Fetches the username and difficulty
     babylog.status('Mining for '+username)
     babylog.status('Using Lower Mining Difficulty: '+UseLowerDiff)
-    global minerobj
-    minerobj=Miner(username,boolUseLowerDiff)
-    minerobj.start_mining()
-    babylog.status('Mining Started!')
+    cpus = os.cpu_count()
+    for i in range(cpus):
+        m = Miner(username, boolUseLowerDiff)
+        m.start_mining()
+        babylog.status('Mining Started on Thread {}!'.format(i))
+        miners.append(m)
     return 'Wooohooo! Mining started! You can close this tab or window.'
 
 
 @app.route('/stop')
 def stop():
     try:
-        minerobj.stop_mining()
-        babylog.status('Stopped Mining!')
+        for m in miners:
+            m.stop_mining()
+            babylog.status('Stopped Mining on thread!')
         return '3 ,2, 1 aaand Done! Mining stopped! You can close this tab or window.'
     except NameError:
         babylog.warn('Tried to stop a non existent miner')
